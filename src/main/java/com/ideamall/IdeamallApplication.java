0b5f3982b2bff6d30a0eb516857410d3cc4e5f2a
lxxjxxki <teddyjk1@naver.com>
2023-11-01 12:14:19 +0900

    Initial commit

---
+++ src/main/java/com/ideamall/IdeamallApplication.java
@@ -0,0 +1,13 @@
+package com.ideamall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeamallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeamallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ideamall/IdeamallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideamall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeamallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
